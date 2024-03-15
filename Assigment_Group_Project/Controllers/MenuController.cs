25a55c0614a6502038c0f76d1c8ff0d791284791
Dang Nhat Huy <113001292+dang-nhat-huy@users.noreply.github.com>
2024-03-15 14:21:35 +0700

    Create Custom Message

--- Assigment_Group_Project/Controllers/MenuController.cs
+++ Assigment_Group_Project/Controllers/MenuController.cs
@@ -7,7 +7,7 @@ namespace Assigment_Group_Project.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class MenuController : Controller
+    public class MenuController : ControllerBase 
     {
         private readonly IMenuService _menuService;
         public MenuController(IMenuService menuService)
@@ -53,5 +53,42 @@ namespace Assigment_Group_Project.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        //[HttpPost("Add")]
+        //[Authorize(Roles = "Staff")]
+        //public IActionResult Add(AddProductRequestVM request)
+        //{
+        //    try
+        //    {
+        //        if (!ModelState.IsValid)
+        //        {
+        //            return BadRequest("Invalid Input");
+        //        }
+        //        var existProduct = _productService.GetAll().Where(x => x.ProductName!.Equals(request.ProductName));
+        //        if (existProduct.Any())
+        //        {
+        //            return BadRequest("There's already product with the same name");
+        //        }
+
+        //        Product newProduct = new Product
+        //        {
+        //            ProductName = request.ProductName,
+        //            Amount = request.Amount,
+        //            Description = request.Description,
+        //            Price = request.Price,
+        //            ReleaseYear = request.ReleaseYear,
+        //            Version = request.Version,
+        //            StatusId = 1,
+        //        };
+        //        _productService.Add(newProduct);
+        //        _productService.Save();
+
+        //        return Ok("Add Successfully");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
     }
 }
