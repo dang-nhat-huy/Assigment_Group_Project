8284b76ced5dc37281670d418448557ecb9d4dab
Haotn1202 <anmay2k1@gmail.com>
2024-03-17 01:51:02 +0700

    git push git add . git commit -m "CRUD Voucher" git switch HaoTN git push -u -f origin HaoTN

--- Service/Service/VoucherService.cs
+++ Service/Service/VoucherService.cs
@@ -1,4 +1,7 @@
-﻿using Service.IService;
+﻿using BusinessObject.Models;
+using Repository;
+using Repository.IRepository;
+using Service.IService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,59 @@ namespace Service.Service
 {
     public class VoucherService : IVoucherService
     {
+        private readonly IVoucherRepository _voucher;
+        public VoucherService(IVoucherRepository voucher)
+        {
+            _voucher = voucher;
+        }
+        public void Add(Voucher Voucher)
+        {
+            _voucher.Add(Voucher);
+        }
+
+        public void Delete(Voucher Voucher)
+        {
+            _voucher.Delete(Voucher);//throw new NotImplementedException();
+        }
+
+        public IEnumerable<Voucher> GetAll(int? page, int? quantity)
+        {
+            const int defaultPage = 1;
+            const int defaultQuantity = 10;
+
+            if (page.HasValue && page <= 0)
+            {
+                page = defaultPage;
+            }
+            if (quantity.HasValue && (quantity <= 0 || quantity > int.MaxValue))
+            {
+                quantity = defaultQuantity;
+            }
+
+            int skip = (page.GetValueOrDefault(defaultPage) - 1) * quantity.GetValueOrDefault(defaultQuantity);
+            return _voucher.GetAll()
+                .Skip(skip)
+                .Take((int)quantity!);//throw new NotImplementedException();
+        }
+
+        public IEnumerable<Voucher> GetAll()
+        {
+            return _voucher.GetAll(); //throw new NotImplementedException();
+        }
+
+        public Voucher? GetById(long id)
+        {
+           return _voucher.GetById(id); //throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _voucher.Save();//throw new NotImplementedException();
+        }
+
+        public void Update(Voucher Voucher)
+        {
+            /_voucher.Update(Voucher)//throw new NotImplementedException();
+        }
     }
 }
